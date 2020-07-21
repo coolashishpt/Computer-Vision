9851039a9bd13741c7e1308cdd96fb7c1c237ab0
Ashish Prasad <40580290+coolashishpt@users.noreply.github.com>
2020-07-21 23:36:10 +0530

    Update model_creation.py

--- Object Detection Yolov3/model_creation.py
+++ Object Detection Yolov3/model_creation.py
@@ -7,6 +7,6 @@ def create_model(weight_path):
     # weight_reader = WeightReader("yolov3.weights")
     weight_reader = WeightReader(weight_path)
     weight_reader.load_weights(model)
-    # model.save("model.h5")
+    # model.save("model.h5") # If want to save the model then just uncomment this line and line 11 both.
     # model_path = "model.h5"
     return model
